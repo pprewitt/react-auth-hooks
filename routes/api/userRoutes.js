bcfb3fbb203952108eea452f46277c7294410e5f
Jay M <jay@suitedconnector.com>
2020-11-10 07:34:32 -0700

    Updated all backend packages and files

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,76 +1,80 @@
-const router = require("express").Router();
-const passport = require("../../config/passport");
-const db = require("../../models");
-const authMiddleware = require("../../config/middleware/authMiddleware");
+/* eslint-disable no-unused-vars */
+const router = require('express').Router();
+const passport = require('../../config/passport');
+const db = require('../../models');
+const authMiddleware = require('../../config/middleware/authMiddleware');
 
-router.post("/login", passport.authenticate("local", {
-  failureRedirect: "/api/users/unauthorized",
-  failureFlash : true
-}), function (req, res, next) {
-  console.log("sign in successful")
-  res.json({
-    user: req.user,
-    loggedIn: true
-  });
-});
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    failureRedirect: '/api/users/unauthorized',
+    failureFlash: true,
+  }),
+  (req, res, next) => {
+    console.log('sign in successful');
+    res.json({
+      user: req.user,
+      loggedIn: true,
+    });
+  },
+);
 
-router.post("/signup", function(req, res, next) {
-  db.User.findOne({username: req.body.username}, function(err, user) {
+router.post('/signup', (req, res, next) => {
+  db.User.findOne({ username: req.body.username }, (err, user) => {
     if (err) throw err;
     if (user) {
-      console.log("user already exists")
-      return res.json("user already exists");
+      console.log('user already exists');
+      return res.json('user already exists');
     }
     if (!user) {
-      db.User.findOne({email: req.body.email}, function(err, useremail) {
-        if (err) throw err;
+      db.User.findOne({ email: req.body.email }, (error, useremail) => {
+        if (error) throw error;
         if (useremail) {
-          return res.json("email is already in use") 
+          return res.json('email is already in use');
         }
         if (!useremail) {
-          let newUser = new db.User({
+          const newUser = new db.User({
             firstname: req.body.firstname,
             lastname: req.body.lastname,
             email: req.body.email,
             username: req.body.username,
-            password: req.body.password
-          })
+            password: req.body.password,
+          });
           newUser.password = newUser.generateHash(req.body.password);
-          newUser.save(function(err) {
-            if (err) throw err;
-            console.log("user saved!");
-            res.redirect(307, "/api/users/login")
+          newUser.save((error2) => {
+            if (error2) throw error2;
+            console.log('user saved!');
+            res.redirect(307, '/api/users/login');
           });
         }
-      })     
+      });
     }
-  })
-});
-
-router.get("/unauthorized", function(req, res, next) {
-  res.json({
-    error: req.flash("error"),
-    message: "user not authenticated"
   });
 });
 
-router.get("/profile", authMiddleware.isLoggedIn, function(req, res, next) {
+router.get('/unauthorized', (req, res, next) => {
   res.json({
-    user: req.user,
-    loggedIn: true
+    error: req.flash('error'),
+    message: 'user not authenticated',
   });
 });
 
-router.get("/logout", authMiddleware.logoutUser, function(req, res, next) {
-  res.json("User logged out successfully");
-});
-
-router.get("/admin", authMiddleware.isAdmin, function(req, res, next) {
+router.get('/profile', authMiddleware.isLoggedIn, (req, res, next) => {
   res.json({
     user: req.user,
-    loggedIn: true
+    loggedIn: true,
   });
 });
 
+router.get('/logout', authMiddleware.logoutUser, (req, res, next) => {
+  res.json('User logged out successfully');
+});
+
+router.get('/admin', authMiddleware.isAdmin, (req, res, next) => {
+  res.json({
+    user: req.user,
+    loggedIn: true,
+  });
+});
 
 module.exports = router;
