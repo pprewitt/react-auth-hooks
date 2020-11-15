1e4e1fac8a7c27a87595de260f372e8ade130562
Jay M <jay@suitedconnector.com>
2020-11-14 21:24:22 -0700

    testing

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose
-  .connect('mongodb+srv://react-auth-hooks-user:wcL0eWhO7U5Xxoxe@react-auth-hooks.9opbf.mongodb.net/react-auth-hooks?retryWrites=true&w=majority', {
+  .connect('mongodb+srv://react-auth-hooks-user:wcL0eWhO7U5Xxoxe@react-auth-hooks.9opbf.mongodb.net/test?retryWrites=true&w=majority', {
     dbName: 'admin',
     useNewUrlParser: true,
     useUnifiedTopology: false,
